df6cd0726e9f221d71e395499c575b53ee95878f
Steve B. <steve@sosp.fr>
2025-02-23 22:16:51 +0100

    Working typing generation

---
+++ shared/src/components/ShinnyButton.module.css.d.ts
@@ -0,0 +1,9 @@
+declare const styles: {
+  readonly "shinnyButton": string;
+  readonly "sub": string;
+  readonly "label": string;
+  readonly "icon": string;
+  readonly "other": string;
+};
+export default styles;
+      
--- shared/vite.config.ts
+++ shared/vite.config.ts
@@ -1,7 +1,51 @@
-import { defineConfig } from "vite";
+import * as fs from "fs";
+import { transform } from "lightningcss";
+import * as path from "path";
+import { defineConfig, type PluginOption } from "vite";
 import dts from "vite-plugin-dts";
 import { externalizeDeps } from "vite-plugin-externalize-deps";
 import { libInjectCss } from "vite-plugin-lib-inject-css";
+
+function cssTypesPlugin(): PluginOption {
+  return {
+    name: "vite-plugin-css-types",
+    enforce: "pre", // Exécuter après le traitement des CSS
+    async transform(code, id) {
+      if (id.endsWith(".css")) {
+        const { exports } = transform({
+          code: Buffer.from(code),
+          filename: id,
+          cssModules: true,
+        });
+
+        if (!exports) return null;
+
+        const target = `${id}.d.ts`;
+
+        const typeDefinitions = Object.keys(exports)
+          .map((key) => `  readonly "${key}": string;`)
+          .join("\n");
+
+        const content = `declare const styles: {
+${typeDefinitions}
+};
+export default styles;
+      `;
+
+        await fs.promises.writeFile(target, content, "utf-8");
+
+        this.info(
+          `Generated types for ${path.relative(
+            __dirname,
+            id
+          )} at ${path.relative(__dirname, target)}`
+        );
+      }
+      return null;
+    },
+  };
+}
+
 export default defineConfig({
   build: {
     lib: {
@@ -12,8 +56,8 @@ export default defineConfig({
     sourcemap: true,
     rollupOptions: {},
   },
-  plugins: [externalizeDeps(), dts(), libInjectCss()],
+  plugins: [externalizeDeps(), dts(), libInjectCss(), cssTypesPlugin()],
   css: {
-    transformer: "lightningcss",
+    transformer: "postcss",
   },
 });
